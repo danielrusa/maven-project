9a720b50dcd04b5384e0a399db5f541202cd671f
druiz_izertis <druiz@izertis.com>
2019-10-29 08:45:14 +0100

    Changes by checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,3 +1,6 @@
+/**
+ * This a package name.
+ */
 package com.example;
 
 /**
@@ -12,8 +15,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   *
+   * @param someone
+   * @return String
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
